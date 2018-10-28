d985884c9e779ff86225e1f923f254f17097fffa
FrezCirno <2277861660@qq.com>
2018-10-28 13:07:38 +0800

    1

--- HeadFile/DS.h
+++ HeadFile/DS.h
@@ -1,16 +1,12 @@
 #ifndef DS_H
 #define DS_H
-
 #define OK 0
 #define ERROR 1
 #define INFEASIBLE -1
 #define OVERFLOW -2
+#define TRUE 1
+#define FALSE 0
 typedef int Status;
 //typedef int ElemType;
-template <typename T> 
-	void swap(T & a, T & b);
-	int Print(ElemType);
-	int PrintLine(ElemType);
-	bool Equal(ElemType, ElemType);
 
 #endif
--- MazePath.cpp
+++ MazePath.cpp
@@ -1,29 +1,8 @@
 #include <iostream>
 using namespace std;
-
-#define MAZESIZE 10
-
-typedef struct {
-	int x;
-	int y;
-} PosType;
-typedef struct {
-	int ord;	  //order
-	PosType pos; //(x,y)
-	dir di;		  //direction
-} PathNode, ElemType;
+#include "MazePath.h"
 #include "HeadFile/Stack_Sq.h"
-typedef int MazeType[MAZESIZE][MAZESIZE];
-
-enum dir {
-	right = 0,
-	down,
-	left,
-	up
-};
-PosType dirpos[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-
-Status MazePath(MazeType maze, PosType start, PosType end, SqStack path) {
+Status MazePath(MazeType maze, PosType start, PosType end, SqStack& path) {
 	PosType curpos = start;
 	PathNode now;
 	int curstep = 1;
@@ -35,12 +14,11 @@ Status MazePath(MazeType maze, PosType start, PosType end, SqStack path) {
 			Book[curpos.x][curpos.y] = 1; //标记
 			now.ord = curstep;
 			now.pos = curpos;
-			now.di = (enum dir)::right;
+			now.di = 0;
 			Push(path, now); //压栈
 
-			if (curpos.x == end.x && curpos.y == end.y) {
-				return (TRUE);    //判断
-			}
+			if (curpos.x == end.x && curpos.y == end.y) return (TRUE);    //判断
+
 
 			curpos.x += dirpos[now.di][0];
 			curpos.y += dirpos[now.di][1];
@@ -52,13 +30,13 @@ Status MazePath(MazeType maze, PosType start, PosType end, SqStack path) {
 				do {
 					Pop(path, now);				  //后退一步
 					Book[now.pos.x][now.pos.y] = 1; //标记不可通
-				} while (now.di == (enum dir)::up && !StackEmpty(path));//如果上一步是向上走的
+				} while (now.di == 3 && !StackEmpty(path));//如果上一步是向上走的
 				//不是向上走的或栈已空
-				if (now.di < (enum dir)::up) {//还没探索过
-					now.di++;//下一方向
+				if (now.di < 3) {//还没探索过
+					now.di ++; //下一方向
 					Push(path, now);
 					curpos.x = now.pos.x + dirpos[now.di][0];
-                    curpos.y = now.pos.y + dirpos[now.di][1];
+					curpos.y = now.pos.y + dirpos[now.di][1];
 				}
 			}
 		}
@@ -67,8 +45,8 @@ Status MazePath(MazeType maze, PosType start, PosType end, SqStack path) {
 	return FALSE;
 }
 int showpath(PathNode n) {
-    cout << n.ord << " " << n.pos.x << " " << n.pos.y << endl;
-    return 0;
+	cout << n.ord << " " << n.pos.x + 1 << " " << n.pos.y + 1 << endl;
+	return 0;
 }
 int main() {
 	int maze[MAZESIZE][MAZESIZE] = {
@@ -87,6 +65,6 @@ int main() {
 	SqStack stack;
 	InitStack(stack);
 	MazePath(maze, s, e, stack);
-    StackTraverse(stack, showpath);
+	StackTraverse(stack, showpath);
 	return 0;
 }
